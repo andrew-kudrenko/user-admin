d109b322a12816ed9bfd82a67540c660b154c0b3
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-12-22 17:18:24 +0300

    [📌 added] Theme has been bound with local storage

--- src/redux/theme/theme-slice.ts
+++ src/redux/theme/theme-slice.ts
@@ -1,16 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { Theme } from '../../interfaces/common.interfaces'
+import { ThemeState } from '../../interfaces/redux.interfaces'
+
+const storedTheme: Theme =
+  JSON.parse(window.localStorage.getItem('theme') ?? 'null') ?? Theme.Light
+
+const initialState: ThemeState = { variant: storedTheme }
 
 const themeSlice = createSlice({
   name: 'theme',
-  initialState: { variant: Theme.Light },
+  initialState,
   reducers: {
     toggleTheme: state => {
       state.variant = state.variant === Theme.Light ? Theme.Dark : Theme.Light
     },
+    setTheme: (state, action) => {
+      state.variant = action.payload
+    },
   },
 })
 
-export const { toggleTheme } = themeSlice.actions
+export const { toggleTheme, setTheme } = themeSlice.actions
 
 export default themeSlice.reducer
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,9 +6,14 @@ import * as colors from '@material-ui/core/colors'
 import { Theme } from '../interfaces/common.interfaces'
 import { useSelector } from 'react-redux'
 import { RootState } from '../interfaces/redux.interfaces'
+import { useEffect } from 'react'
+import { useLocalStorage } from '../hooks/local-storage.hook'
 
 export const useCustomTheme = (): MaterialTheme => {
   const { variant } = useSelector((state: RootState) => state.theme)
+  const [, setStoredTheme] = useLocalStorage<Theme>('theme', variant)
+
+  useEffect(setStoredTheme.bind(null, variant), [variant])
 
   return createMuiTheme({
     palette: {
